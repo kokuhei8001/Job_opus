c7139dd77092a1ddca7eaac3e0cd31622b77b172
kokuhei8001 <kokuhei8001@gmail.com>
2019-01-22 13:34:36 +0900

    プレイヤーが障害物の後ろにいたら追跡しないコードを書いたが、なぜか動かない。 他のプロジェクトで全く同じコード、同じ設定だと動く

--- Assets/Enemy/enemy_script.cs
+++ Assets/Enemy/enemy_script.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class enemy_script : MonoBehaviour {
 
-    private bool hunt = false; //追いかけているか
-    private PlayerMove _player;//プレイヤーの情報
+    private bool IsSearching = false; //追いかけているか
+    private GameObject _player;//プレイヤーの情報
     private Vector3 player_pos;       //プレイヤーの位置情報
     private Vector3 myself_pos; //自分の位置情報
-
-
-
+    
     private Rigidbody rb;
-    public LayerMask mask;
 
     private void Start()
     {
@@ -21,32 +18,70 @@ public class enemy_script : MonoBehaviour {
 
     private void Update()
     {
-        if(hunt == true)
+        if (IsSearching)
         {
-           player_pos = _player.transform.position;
-           myself_pos = transform.position;
-           player_pos = new Vector3(player_pos.x, myself_pos.y, player_pos.z);
-           
-           transform.LookAt(player_pos);
-           rb.MovePosition(transform.position + transform.forward * Time.deltaTime);
+            player_pos = _player.transform.position;
+            if(SearchRay())
+            {
+                RunToPlayer();
+            }
+        }
+
+        //デバック
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            transform.position = new Vector3(5, 1, 7);
         }
     }
 
+    //範囲内に入った瞬間
     private void OnTriggerEnter(Collider other)
     {
-        var player = other.gameObject.GetComponent<PlayerMove>();
-        if (player != null)
+        if (other.gameObject.tag == "Player")
         {
-            _player = player;
-            hunt = true;
+            _player = other.gameObject;
+            IsSearching = true;
         }
     }
+    //範囲から出た瞬間
     private void OnTriggerExit(Collider other)
     {
-        var player = other.gameObject.GetComponent<PlayerMove>();
-        if (player != null)
+        if (other.gameObject.tag == "Player")
         {
-            hunt = false;
+            IsSearching = false;
         }
+    }
+    //エネミーからプレイヤーが見えているかどうか
+    private bool SearchRay()
+    {
+        Vector3 direction = player_pos - transform.position;
+        Ray ray = new Ray(transform.position, direction);
+        RaycastHit hit;
+
+        Debug.DrawRay(transform.position, direction, Color.red);
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            if (hit.collider.tag == "Player")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return false;
+    }
+    //プレイヤーを追跡している状態
+    private void RunToPlayer()
+    {
+        Debug.Log("RunToPlayer");
+        myself_pos = transform.position;
+        Vector3 target_pos = new Vector3(player_pos.x, myself_pos.y, player_pos.z);
+        Debug.Log("LookAtまえ！");
+        transform.LookAt(target_pos);//プレイヤーの方を向く
+        Debug.Log("LookAtあと！");
+        rb.MovePosition(myself_pos + transform.forward * Time.deltaTime);//前方に進む
     }
 }
